e50000c0643e3ab91661d9cd81d09d3609871e85
Austin <joha0033@gmail.com>
2018-01-16 10:50:44 -0700

    nav comp bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 
 import { environment } from './../environments/environment';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
